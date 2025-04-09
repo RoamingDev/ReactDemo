b8901d00117e731b72bb1a1e142ec73e4ca48a28
Roaming <62654209+RoamingDev@users.noreply.github.com>
2025-04-09 12:12:52 -0300

    Section con Forwarding props

--- websiteReact/src/components/TabButton.jsx
+++ websiteReact/src/components/TabButton.jsx
@@ -2,10 +2,10 @@
 
 //onSelect es basicamente la propiedad onClick, pero 
 //le cambiamos de nombre para hacer notar que se puede poner el nombre que querramos
-export default function TabButton({children, onSelect, isSelected}) {
+export default function TabButton({children, isSelected, ...props}) {
   return (
     <li>
-      <button className={isSelected ? "active" : undefined } onClick= {onSelect}>{
+      <button className={isSelected ? "active" : undefined } {...props}>{
         children}
       </button>
     </li>
