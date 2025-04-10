fe39d3f14193910dc0e2d971aa8641fed77709dc
Roaming <62654209+RoamingDev@users.noreply.github.com>
2025-04-09 21:33:32 -0300

    Settear el tipo de componente dinamicamente

--- websiteReact/src/components/Tabs.jsx
+++ websiteReact/src/components/Tabs.jsx
@@ -1,8 +1,9 @@
-export default function Tabs({children, buttons}){
+export default function Tabs({children, buttons, ButtonsContainer}){
+    //const ButtonsContainer = buttonsContainer; //Hay que hacer esto xq si escribiera <buttonsContainer> buscaría un built-in component con ese nombre en vez de tomar la variable y tratarlo como un CustomComponent
     return <>
-        <menu>
+        <ButtonsContainer>
             {buttons}
-        </menu>
+        </ButtonsContainer>
         {children}
     </>
 }
