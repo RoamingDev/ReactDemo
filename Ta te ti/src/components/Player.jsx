0b41d2a3855ed2f800b9e1cacc3110e66aac3a85
Roaming <62654209+RoamingDev@users.noreply.github.com>
2025-04-16 12:49:56 -0300

    Tablero base

--- Ta te ti/src/components/Player.jsx
+++ Ta te ti/src/components/Player.jsx
@@ -1,11 +1,29 @@
-export default function Player({ name, symbol }) {
-  return (
-    <li>
-      <span>
-        <span className="player-name">{name}</span>
-        <span className="player-symbol">{symbol}</span>
-      </span>
-      <button>Edit</button>
-    </li>
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditing(){
+        setIsEditing((editing) => !editing); 
+    }
+
+    function handleChange(event){
+        setPlayerName((event.target.value));
+    }
+
+    let editablePlayerName = <span className="player-name" >{playerName}</span>;
+    if (isEditing) {
+        editablePlayerName = (<input type="text" required value={playerName} onChange={handleChange} />);
+    }
+
+    return (
+        <li>
+        <span className="player">
+            {editablePlayerName}
+            <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditing}>{isEditing ? "Save" : "Edit"}</button>
+        </li>
   );
 }
